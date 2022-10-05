23a96b96063a87add1debb3f5d989838a10dc357
KoganScherer37 <koganscherer23@gmail.com>
2022-10-05 13:57:54 -0500

    question 5 for review

--- Lahman_question_5.sql
+++ Lahman_question_5.sql
@@ -3,21 +3,21 @@
 --Round the numbers you report to 2 decimal places. 
 -- B.Do the same for home runs per game. Do you see any trends?
 
-SELECT DISTINCT CASE WHEN p.yearid < 1930 THEN '1920s'
-            WHEN p.yearid < 1940 THEN '1930s'
-			WHEN p.yearid < 1950 THEN '1940s'
-			WHEN p.yearid < 1960 THEN '1950s'
-			WHEN p.yearid < 1970 THEN '1960s'
-			WHEN p.yearid < 1980 THEN '1970s'
-			WHEN p.yearid < 1990 THEN '1980s'
-			WHEN p.yearid < 2000 THEN '1990s'
-			WHEN p.yearid < 2010 THEN '2000s'
-			WHEN p.yearid < 2020 THEN '2010s' END AS decades,
-			SUM(p.so)/SUM(p.g) AS avg_so_per_game,
-			SUM(b.hr)/SUM(b.g) AS avg_hr_per_game
-FROM pitching AS p
-INNER JOIN batting AS b
-USING(playerid)
-WHERE p.yearid >= '1920'
+SELECT 
+       CASE WHEN yearid < 1930 THEN '1920s'
+            WHEN yearid < 1940 THEN '1930s'
+			WHEN yearid < 1950 THEN '1940s'
+			WHEN yearid < 1960 THEN '1950s'
+			WHEN yearid < 1970 THEN '1960s'
+			WHEN yearid < 1980 THEN '1970s'
+			WHEN yearid < 1990 THEN '1980s'
+			WHEN yearid < 2000 THEN '1990s'
+			WHEN yearid < 2010 THEN '2000s'
+			WHEN yearid < 2020 THEN '2010s' END AS decades,
+			ROUND(SUM(so::decimal)/(SUM(g)/2),2) AS avg_so_per_game,
+			ROUND(SUM(hr::decimal)/(SUM(g)/2),2) AS avg_hr_per_game
+FROM teams
+WHERE yearid >= '1920'
 GROUP BY decades
 ORDER BY decades;
+
