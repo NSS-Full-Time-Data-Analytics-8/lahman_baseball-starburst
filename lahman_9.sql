0f96b992d62eaa05d7f7a4fa2250f074e7a8cd2f
lishabates <112971059+lishabates@users.noreply.github.com>
2022-10-05 10:13:03 -0500

    Delete lahman_9.sql

--- lahman_9.sql
+++
@@ -1,8 +0,0 @@
---9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? 
---Give their full name and the teams that they were managing when they won the award.
-
-SELECT p.namefirst, p.namelast, am.lgid
-FROM AwardsManagers as am
-INNER JOIN people as p
-USING (playerid)
-WHERE am.awardid = 'TSN Manager of the Year' AND am.lgid = 'AL' AND am.lgid = 'NL'
