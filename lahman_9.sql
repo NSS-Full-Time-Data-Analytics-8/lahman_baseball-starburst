c7378082582db086808cb7c6dc5c6962b5feeb99
lishabates <112971059+lishabates@users.noreply.github.com>
2022-10-06 12:04:05 -0500

    question 9 push

--- lahman_9.sql
+++ lahman_9.sql
@@ -1,8 +1,35 @@
 --9. Which managers have won the TSN Manager of the Year award in both the National League (NL) and the American League (AL)? 
 --Give their full name and the teams that they were managing when they won the award.
 
-SELECT p.namefirst, p.namelast, am.lgid
-FROM AwardsManagers as am
-INNER JOIN people as p
+WITH AL
+		AS (SELECT p.namefirst, p.namelast, am.lgid 
+				FROM AwardsManagers as am
+				INNER JOIN people as p
+				USING (playerid)
+				WHERE am.lgid = 'AL'),
+				
+NL 		AS (SELECT p.namefirst, p.namelast, am.lgid 
+				FROM AwardsManagers as am
+				INNER JOIN people as p
+				USING (playerid)
+				WHERE am.lgid = 'NL'),
+Team	AS (select p.playerid, t.name
+		   from people as p
+		   INNER JOIN managershalf as m
+		   USING (playerid)
+		   INNER JOIN teams as t
+		   USING (teamid))
+		   
+
+SELECT DISTINCT P.namefirst, p.namelast, am.yearid, name
+FROM Awardsmanagers as am
+INNER JOIN people as p 
 USING (playerid)
-WHERE am.awardid = 'TSN Manager of the Year' AND am.lgid = 'AL' AND am.lgid = 'NL'
+INNER JOIN AL
+USING (namefirst)
+INNER JOIN NL
+USING (namefirst)
+INNER JOIN Team 
+USING (playerid)
+WHERE am.awardid = 'TSN Manager of the Year'
+AND Awardsmanagers.lgid 'NL' AND Awardsmanagers.lgid 'AL'
