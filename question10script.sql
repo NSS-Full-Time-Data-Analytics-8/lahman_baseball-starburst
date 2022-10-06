9757dd9ff05231c934d62c18448993c54c30f9b7
scost46 <scost46@gmail.com>
2022-10-06 16:07:54 -0500

    question 10 final push

--- question10script.sql
+++ question10script.sql
@@ -1,25 +1,29 @@
---question 10
---Find all players who hit their career highest number of home runs in 2016.
---Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. 
---Report the players' first and last names and the number of home runs they hit in 2016.
-WITH total_seasons AS
-(SELECT (EXTRACT(year FROM finalgame::date))-(EXTRACT(year FROM debut::date))+1
-FROM people
-WHERE (EXTRACT(year FROM finalgame::date))-(EXTRACT(year FROM debut::date))+1 >= 10)
+WITH max_hr AS (SELECT playerid,
+	  				   MAX(hr) AS hr
+					   FROM batting
+				       GROUP BY playerid
+					   ORDER BY hr DESC),
+					   
+	season_count AS (SELECT (EXTRACT(year FROM finalgame::date))-(EXTRACT(year FROM debut::date))+1 AS seas_count,
+					 playerid
+				 FROM people
+      			 WHERE (EXTRACT(year FROM finalgame::date))-(EXTRACT(year FROM debut::date))+1 >= 10)
 
 SELECT namefirst, 
-	   namelast, 
-	   MAX(hr) AS max_hr, 
-	   yearid AS year
-FROM batting
-INNER JOIN people
-USING(playerid)
-WHERE (hr >= 1)
-AND yearid = '2016'
-GROUP BY namefirst, namelast, hr, yearid, debut, finalgame
-ORDER BY hr DESC
-
-
+	   namelast,
+	   b.playerid,
+	   hr
+FROM batting AS b
+		INNER JOIN people
+		USING(playerid)
+		INNER JOIN max_hr
+		USING(playerid,hr)
+		INNER JOIN season_count
+		USING(playerid)
+WHERE hr >= 1
+AND b.yearid = '2016'
+GROUP BY namefirst, namelast, hr, b.yearid,b.playerid
+ORDER BY hr DESC;
 
 
 
